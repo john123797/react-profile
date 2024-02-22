45c9c159e29fffa2efe44b475574532e606d1d55
john123797 <john456797@gmail.com>
2024-02-22 14:38:41 +0800

    Update project ownership details and remove LICENSE

--- src/app/pages/links.tsx
+++ src/app/pages/links.tsx
@@ -10,31 +10,31 @@ export const links = [
   {
     index: 0,
     title: "Find me on Github",
-    href: "https://github.com/noworneverev",
+    href: "https://github.com/john123797",
     icon: <FaGithub />,
   },
   {
     index: 1,
     title: "Find me on LinkedIn",
-    href: "https://www.linkedin.com/in/yan-ying-liao/",
+    href: "https://www.linkedin.com/in/john123797/",
     icon: <FaLinkedin />,
   },
   {
     index: 2,
     title: "Contact me via email",
-    href: "mailto:n9102125@gmail.com",
+    href: "mailto:john456797@gmail.com",
     icon: <FaEnvelope />,
   },
   {
     index: 3,
     title: "Find me on Medium",
-    href: "https://medium.com/@noworneverev",
+    href: "",
     icon: <FaMedium />,
   },
   {
     index: 4,
     title: "Find me on Blog",
-    href: "https://noworneverev.github.io/blog/",
+    href: "",
     icon: <FaBlog />,
   },
 ];
